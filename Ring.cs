e6535f6d4be5ca36fda9a95eb50b661b29f076d3
Frank <frank90086@hotmail.com>
2019-01-30 13:20:17 +0800

    Create Ring.cs

---
+++ Ring.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+namespace Tutorial
+{
+    public class Ring
+    {
+        private IEvtHandler _handler;
+        public Ring(IEvtHandler handler){
+            _handler = handler;
+        }
+        public void OnSendMessage(Func<List<string>, List<People>> family, List<string> names, string msg){
+            List<People> fs = family(names);
+            foreach (People f in fs)
+                _handler.Evt += f.Do;
+
+            _handler.Do(new EvtArgs(msg));
+            Console.WriteLine("Finish!");
+        }
+
+        public void OnSendMessage(Func<Dictionary<int, string>, List<People>> student, Dictionary<int, string> stDic, string msg){
+            List<People> sts = student(stDic);
+            foreach (People st in sts)
+                _handler.Evt += st.Do;
+
+            _handler.Do(new EvtArgs(msg));
+            Console.WriteLine("Finish!");
+        }
+    }
+}
