d2f2fc0f1ebe657f0df456be3e6b4582e45fd403
Frank <frank90086@hotmail.com>
2019-01-31 17:48:24 +0800

    Create FuncMethod.cs

---
+++ FuncMethod.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Tutorial
+{
+    public static class FuncMethod
+    {
+        public static Func<T, TResult> GetFunc<T, TResult>(string methodName)
+        {
+            return null;
+        }
+    }
+}
